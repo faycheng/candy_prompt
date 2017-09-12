2a2fd483c2b1ccde5228c0b0a39d03f617b8657f
程飞 <fay.cheng.cn@gmail.com>
2017-09-13 00:22:22 +0800

    remove convertor

--- candy_prompt/prompt.py
+++ candy_prompt/prompt.py
@@ -13,27 +13,17 @@ from candy_prompt.validator import *
 from candy_prompt.history import PromptFileHistory as FileHistory
 from enum import Enum
 from candy import path
-from candy.utils.convertor import str2boo
-
-
-class Converter(object):
-    def __init__(self, convert_func, validator):
-        self.convert_func = convert_func
-        self.validator = validator
-
-    def convert(self, data):
-        return self.convert_func(data)
 
 
 class PromptType(Enum):
-    STR = Converter(str, StrValidator())
-    INT = Converter(int, IntValidator())
-    BOOL = Converter(str2boo, BoolValidator())
-    FLOAT = Converter(float, FloatValidator())
-    LIST = Converter(ast.literal_eval, ListValidator())
-    DICT = Converter(ast.literal_eval, ListValidator())
-    DIR = Converter(str, DirValidator())
-    FILE = Converter(str, FileValidator())
+    STR = StrValidator()
+    INT = IntValidator()
+    BOOL = BoolValidator()
+    FLOAT = FloatValidator()
+    LIST = ListValidator()
+    DICT = ListValidator()
+    DIR = DirValidator()
+    FILE = FileValidator()
 
 
 def prompt(message, type='STR', default=None, multiline=False, history=None):
@@ -41,8 +31,8 @@ def prompt(message, type='STR', default=None, multiline=False, history=None):
     converter = getattr(PromptType, type)
     completer = WordCompleter(words=[], history=history)
     res = prompt_toolkit.prompt(message, default=default or '', history=history,
-                                validator=converter.value.validator, completer=completer, multiline=multiline)
-    return converter.value.convert(res)
+                                validator=converter.value, completer=completer, multiline=multiline)
+    return ast.literal_eval(res)
 
 
 def prompt_list(message, type='STR', default=None, completions=None, multiline=False, history=None):
@@ -50,15 +40,13 @@ def prompt_list(message, type='STR', default=None, completions=None, multiline=F
     converter = getattr(PromptType, type)
     completer = WordCompleter(words=completions, history=history)
     res = prompt_toolkit.prompt(message, default=default or '', history=history,
-                                validator=converter.value.validator, completer=completer, multiline=multiline)
-    return converter.value.convert(res)
+                                validator=converter.value, completer=completer, multiline=multiline)
+    return ast.literal_eval(res)
 
 
 def prompt_path(message, root, type='DIR', recursion=False, default=None):
     converter = getattr(PromptType, type)
     completer = PathCompleter(root, match_type=type, recursion=recursion)
     res = prompt_toolkit.prompt(message, default=default or '', completer=completer,
-                                validator=converter.value.validator)
-    return converter.value.convert(res)
-
-
+                                validator=converter.value)
+    return ast.literal_eval(res)
